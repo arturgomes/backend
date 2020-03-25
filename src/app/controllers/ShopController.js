bbd7a3c3342bc1f717306b044da7b5dce685a512
Artur Oliveira Gomes <artur@MacBook-Pro.local>
2020-03-25 14:55:55 -0400

    url shortner

--- src/app/controllers/ShopController.js
+++ src/app/controllers/ShopController.js
@@ -2,27 +2,7 @@ import * as Yup from 'yup';
 import Shop from '../models/Shop';
 import Error from '../errors/errors';
 
-
 class ShopController {
-
-  getrandom() {
-    let text = "";
-    const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-    for (let i = 0; i < 5; i++)
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-    return text;
-  }
-
-  async index(req, res) {
-    const { shop_id } = req.params;
-
-    const shop = await Shop.findOne({ where: { id: shop_id } });
-    if (shop) {
-      return res.json(shop.name);
-    }
-    return res.json({ error: 'Shop not found' });
-  }
   async store(req, res) {
     const schema = Yup.object().shape({
       name: Yup.string().required(),
@@ -49,23 +29,22 @@ class ShopController {
       return res.status(400).json({ error: Error.validation_failed });
     }
 
-    // let valid_url = true;
-    // let ur;
-    // while (valid_url !== false){
-    //   ur = this.getrandom();
-    //   const shop_ur = await Shop.findOne({ where: { short_url: ur } });
-    //   if(!shop_ur){
-    //     valid_url = false;
-    //   }
-    // }
-    // console.log(ur);
+    // const shopExists = await Shop.findOne({
+    //   where: { cnpj: req.body.cnpj },
+    // });
 
+    // if (shopExists) {
+    //   return res.status(400).json({ error: Error.shop_exists });
+    // }
+    // const usr = await User.findOne({ where: { id: req.userId } });
+    // if (usr) {
+    //   return res.status(400).json({ error: Error.user_cannot_create_shop });
+    // }
     const { id, name, manager, phone } = await Shop.create({
       name: req.body.name,
       phone: req.body.phone,
       manager: req.body.manager,
       retail_id: req.body.retail_id,
-      // short_url:ur
     });
 
     return res.json({ id, name, manager, phone });
