69e49ecf25b922c47a99bcd7f74d803e8303f81a
Artur Oliveira Gomes <artur@MacBook-Pro.local>
2020-03-31 17:07:27 -0400

    url shortner 3

--- src/app/controllers/QrController.js
+++ src/app/controllers/QrController.js
@@ -2,14 +2,15 @@ import Shop from '../models/Shop';
 
 class QrController {
   async index(req, res) {
-    const shops = await Shop.findOne(
+    const shops = await Shop.findAll(
       { attributes: ['id', 'retail_id', 'name', 'short_url'] },
       { where: { retail_id: req.body.retail_id } }
-    )
-      .map(el => el.get({ plain: true }))
-      .filter(s => s.retail_id === req.body.retail_id);
+      );
+      // .map(el => el.get({ plain: true }))
+      // .filter(s => s.retail_id === req.body.retail_id);
 
-    if (shops !== null) return res.json(shops);
+      console.log(shops);
+      if (shops !== null) return res.json(shops);
     // if (fbs) {
     //   return res.json(fbs);
     // }
