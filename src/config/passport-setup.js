69a38cc4bb87aaea29f7b2749f4abf11b36ed13c
Artur Oliveira <artur.o.gomes@gmail.com>
2020-07-16 19:02:37 +0000

    yy

--- src/config/passport-setup.js
+++
@@ -1,4 +0,0 @@
-import passport from "passport";
-const keys = require("./keys");
-import User from "../app/models/User";
-
--- src/config/sociallogin.js
+++
@@ -1,8 +0,0 @@
-const FACEBOOK = {
-  clientId:"307286726964664",
-  clientSecret:"4fd0e8f33743e02781773532fcad1141"
-}
-
-module.exports = {
-  FACEBOOK
-}
