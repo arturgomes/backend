13e9d09a7027707e7e83fe3bf8bc9ea44a5293ec
Artur Oliveira Gomes <artur@MacBook-Pro.local>
2020-07-22 15:46:57 -0400

    social login backend - 22072020 - working

--- src/app/controllers/SocialSessionController.js
+++ src/app/controllers/SocialSessionController.js
@@ -7,7 +7,7 @@ import Error from '../errors/errors';
 
 class SocialSessionController {
   async store(req, res) {
-    const user = await User.findOne({ where: { email } });
+    const user = await User.findOne({ where: { email:req.user.dataValues.email } });
     if (!user) {
       return res.status(401).json({ error: Error.user_not_found });
     }
@@ -17,9 +17,9 @@ class SocialSessionController {
         success: true,
         message: "user has successfully authenticated",
         login: {
-          user_id: req.user.user_id, //pass in the id and displayName params from Facebook
-          name: req.user.name,
-          email: req.user.email,
+          user_id: req.user.dataValued.user_id, //pass in the id and displayName params from Facebook
+          name: req.user.dataValued.name,
+          email: req.user.dataValued.email,
           tu: 'b026324c6904b2a9cb4b88d6d61c81d1',
         },
         token: jwt.sign({ id }, authConfig.secret, {
