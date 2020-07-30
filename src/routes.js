bd122263a6fc0a826a9c57c0d6e1148e253fb4c5
Artur Oliveira Gomes <>
2020-07-30 18:57:24 -0400

    social login - 30072020

--- src/routes.js
+++ src/routes.js
@@ -40,6 +40,7 @@ const upload = multer(multerConfig);
 
 routes.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "https://couponfeed.co");
+  res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
