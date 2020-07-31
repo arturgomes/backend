2a7b9e29fc16190af0ba72e52ad334c1b4d94aca
Artur Oliveira Gomes <>
2020-07-31 10:24:58 -0400

    social login - 31072020

--- src/app.js
+++ src/app.js
@@ -51,10 +51,10 @@ class App {
       // )
       (req,res,next) =>{
         res.header("Access-Control-Allow-Origin","*");
-        res.header('Access-Control-Allow-Credentials','true');
         res.header("Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept, Authorization");
         if(req.method === 'OPTIONS'){
+          res.header('Access-Control-Allow-Credentials','true');
           res.header('Access-Control-Allow-Methods', "PUT, POST, PATCH, DELETE, GET");
           return res.status(200).json({});
         }
