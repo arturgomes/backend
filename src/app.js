782688cfac3e658cfbde408633ed85eb061466a2
Artur Oliveira Gomes <>
2020-08-10 16:27:41 -0400

    trying to fix cors

--- src/app.js
+++ src/app.js
@@ -70,7 +70,7 @@ class App {
     this.server.use(function (req, res, next) {
       // console.log(req.headers);
       res.header('Access-Control-Allow-Headers', "Access-Control-Allow-Origin,Access-Control-Allow-Credentials,Content-Type,Authorization,X-Requested-With");
-      res.header('Access-Control-Allow-Origin', 'https://www.couponfeed.co');
+      // res.header('Access-Control-Allow-Origin', 'https://www.couponfeed.co');
       res.header('Access-Control-Allow-Credentials', true);
       res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, HEAD');
       next();
