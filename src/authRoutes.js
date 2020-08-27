6904287f2573c5466d604832e1b71b9149264f8a
Artur Oliveira Gomes <>
2020-08-27 15:47:53 -0400

    fixing between login of retail and customer

--- src/authRoutes.js
+++ src/authRoutes.js
@@ -10,10 +10,8 @@ const routes = new Router();
 
 routes.get('/success', (req, res) => {
   // console.log("entrou no /success")
-  // console.log(req.user);
-  // if (req.session.retail==="true") {
-    console.log(req.user.hasOwnProperty('cnpj'))
-    if(req.user.hasOwnProperty('cnpj')){
+  console.log(req.user);
+  if (req.session.retail==="true") {
     // console.log(req.user);
     const { id, name } = req.user;
     // console.log(req.user);
