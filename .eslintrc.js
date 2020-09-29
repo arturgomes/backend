43e3827285e179fb49948708bafad01fd31275e9
Artur Oliveira Gomes <>
2020-09-29 16:12:12 -0400

    quick commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,29 @@
-// module.exports = {
-//   env: {
-//     es6: true,
-//     node: true,
-//   },
-//   extends: [
-//     'airbnb-base',
-//     'prettier'
-//   ],
-//   plugins: ['prettier'],
-//   globals: {
-//     Atomics: 'readonly',
-//     SharedArrayBuffer: 'readonly',
-//   },
-//   parserOptions: {
-//     ecmaVersion: 2018,
-//     sourceType: 'module',
-//   },
-//   rules: {
-//     "prettier/prettier": "error",
-//     "class-methods-use-this": "off",
-//     "no-param-reassign": "off",
-//     "camelcase": "off",
-//     "no-unused-vars": ["error", { "argsIgnorePattern": "next" }],
-//     "rules": {
-//       "node/file-extension-in-import": ["error", "never"],
-//     }
-//   }
-// };
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'prettier'
+  ],
+  plugins: ['prettier'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    "prettier/prettier": "error",
+    "class-methods-use-this": "off",
+    "no-param-reassign": "off",
+    "camelcase": "off",
+    "no-unused-vars": ["error", { "argsIgnorePattern": "next" }],
+    // "rules": {
+    "node/file-extension-in-import": ["error", "never"],
+    // }
+  }
+};
