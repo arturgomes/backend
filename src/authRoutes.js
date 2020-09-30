ec2818723eea462b45521a950e53a38da7dc0ae6
Artur Oliveira Gomes <>
2020-09-30 09:34:43 -0400

    quick commit

--- src/authRoutes.js
+++ src/authRoutes.js
@@ -21,6 +21,8 @@ routes.get('/success', (req, res) => {
     // console.log(req.user);
     const { id, name } = req.user.dataValues;
     // console.log(req.user);
+    const tk = jwt.sign({ id }, authConfig.secret, { expiresIn: authConfig.expiresIn, });
+    console.log(tk);
     const response = {
       success: true,
       message: "retail has successfully authenticated",
@@ -32,9 +34,7 @@ routes.get('/success', (req, res) => {
         tu: 'b026324c6904b2a9cb4b88d6d61c81d1',
       },
       cookies: req.cookies,
-      token: jwt.sign({ id }, authConfig.secret, {
-        expiresIn: authConfig.expiresIn,
-      }),
+      token: tk
     };
     console.log({resp: response})
     return res.status(200).json(response);
