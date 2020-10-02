5a181b165e4a8fa47119b89b1e7d0e3a330874ec
Artur Oliveira Gomes <>
2020-10-01 23:47:45 -0400

    quick commit

--- src/app/controllers/DisplayFeedbackController.js
+++ src/app/controllers/DisplayFeedbackController.js
@@ -1,94 +1,142 @@
 // import * as Yup from 'yup';
-import Feedback from '../models/Feedback.js';
-import Shop from '../models/Shop.js';
+import Feedback from '../models/Feedback';
+import Shop from '../models/Shop';
 
 // import Error from '../errors/errors';
 // essa porra de display tá dando nos nervos, depois eu continuo
 class DisplayFeedbackController {
   async index(req, res) {
-    console.log("entrou no controller", req.body.retail_id);
-    // const shops = await Shop.findAll(
-    //   { attributes: ['id', 'retail_id', 'name'] },
-    //   { where: { retail_id: req.body.retail_id } }
-    // )
-    //
-    // console.log(shops);
-    let shopls;
-    await Shop.findAll(
-      { attributes: ['id', 'retail_id', 'name'] },
-      { where: { retail_id: req.body.retail_id } }
-    ).then(shops => {
-      const list = shops.map(el => el.get({ plain: true }))
-        .filter(s => s.retail_id === req.body.retail_id);
-      console.log(list);
-      if (list.length === 0) {
-        res.json([])
+    const shops = await Shop.findAll(
+      {
+        attributes: ['id', 'retail_id', 'name'],
+        where: { retail_id: req.body.retail_id }
       }
-      shopls = list;
-
-    })
-      .catch(err => console.log(err))
-    //   .map(el => el.get({ plain: true }))
-    //   .filter(s => s.retail_id === req.body.retail_id);
+    )
+      // .map(el => el.get({ plain: true }))
+      // .filter(s => s.retail_id === req.body.retail_id);
     // console.log(shops);
 
-    // const fb = await Promise.all(
-    //   shops.map(async s => {
-    //     const { id } = s;
-    //     // console.log(id);
-    //     const f = await Feedback.findAll(
-    //       {
-    //         attributes: ['date', 'shop_id', 'nps_value', 'comment_optional'],
-    //       },
-    //       { where: { shop_id: id } }
-    //     )
-    //       .map(el => el.get({ plain: true }))
-    //       .filter(s => s.shop_id === id);
-    //     // console.log(f);
+    const fb = await Promise.all(
+      shops.map(async s => {
+        const { id } = s;
+        // console.log(id);
+        const f = await Feedback.findAll(
+          {
+            attributes: ['date', 'shop_id', 'nps_value', 'comment_optional'],
+            where: { shop_id: id }
+          }
+        )
+          // .map(el => el.get({ plain: true }))
+          // .filter(s => s.shop_id === id);
+        // console.log(f);
 
-    //     return { shop_name: s.name, f };
-    //   })
-    // );
+        return { shop_name: s.name, f };
+      })
+    );
+    // console.log(fb);
 
-    const functionWithPromise = id => { //a function that returns a promise
-      Feedback.findAll(
-        {
-          attributes: ['date', 'shop_id', 'nps_value', 'comment_optional'],
-        },
-        { where: { shop_id: id } }
-      ).then( fb => {
-        const fbs = fb.map(el => el.get({ plain: true }))
-        .filter(s => s.shop_id === id);
-        console.log(fbs)
-        return fbs
-      }
-      ).catch(err => console.log(err))
-    }
-    const anAsyncFunction = async item => {
-      const {id } = item
-      return functionWithPromise(id)
-    }
-    const getData = async (shops) => {
-      return Promise.all(shops.flatMap(item => [anAsyncFunction(item)]))
-    }
-
-    getData(shopls).then(data => {
-      // console.log(data)
-      if (data !== null) return res.json(data);
+    if (fb !== null) return res.json(fb);
     // if (fbs) {
     //   return res.json(fbs);
     // }
     return res.json({ error: 'Shop not found' });
-    }).catch(err => console.log(err))
-    // console.log(fb);
-    console.log(fb);
-
-    // if (fb !== null) return res.json(fb);
-    // // if (fbs) {
-    // //   return res.json(fbs);
-    // // }
-    // return res.json({ error: 'Shop not found' });
   }
 }
 
 export default new DisplayFeedbackController();
+
+
+
+// // import * as Yup from 'yup';
+// import Feedback from '../models/Feedback.js';
+// import Shop from '../models/Shop.js';
+
+// // import Error from '../errors/errors';
+// // essa porra de display tá dando nos nervos, depois eu continuo
+// class DisplayFeedbackController {
+//   async index(req, res) {
+//     console.log("entrou no controller", req.body.retail_id);
+//     // const shops = await Shop.findAll(
+//     //   { attributes: ['id', 'retail_id', 'name'] },
+//     //   { where: { retail_id: req.body.retail_id } }
+//     // )
+//     //
+//     // console.log(shops);
+//     let shopls;
+//     await Shop.findAll(
+//       { attributes: ['id', 'retail_id', 'name'],
+//       where: { retail_id: req.body.retail_id } }
+//     ).then(shops => {
+//       const list = shops.map(el => el.get({ plain: true }))
+//         .filter(s => s.retail_id === req.body.retail_id);
+//       console.log(list);
+//       if (list.length === 0) {
+//         res.json([])
+//       }
+//       shopls = list;
+
+//     })
+//       .catch(err => console.log(err))
+//     //   .map(el => el.get({ plain: true }))
+//     //   .filter(s => s.retail_id === req.body.retail_id);
+//     // console.log(shops);
+
+//     // const fb = await Promise.all(
+//     //   shops.map(async s => {
+//     //     const { id } = s;
+//     //     // console.log(id);
+//     //     const f = await Feedback.findAll(
+//     //       {
+//     //         attributes: ['date', 'shop_id', 'nps_value', 'comment_optional'],
+//     //       },
+//     //       { where: { shop_id: id } }
+//     //     )
+//     //       .map(el => el.get({ plain: true }))
+//     //       .filter(s => s.shop_id === id);
+//     //     // console.log(f);
+
+//     //     return { shop_name: s.name, f };
+//     //   })
+//     // );
+
+//     const functionWithPromise = id => { //a function that returns a promise
+//       Feedback.findAll(
+//         {
+//           attributes: ['date', 'shop_id', 'nps_value', 'comment_optional'],
+//         where: { shop_id: id } }
+//       ).then( fb => {
+//         const fbs = fb.map(el => el.get({ plain: true }))
+//         .filter(s => s.shop_id === id);
+//         console.log(fbs)
+//         return fbs
+//       }
+//       ).catch(err => console.log(err))
+//     }
+//     const anAsyncFunction = async item => {
+//       const {id } = item
+//       return functionWithPromise(id)
+//     }
+//     const getData = async (shops) => {
+//       return Promise.all(shops.flatMap(item => [anAsyncFunction(item)]))
+//     }
+
+//     getData(shopls).then(data => {
+//       // console.log(data)
+//       if (data !== null) return res.json(data);
+//     // if (fbs) {
+//     //   return res.json(fbs);
+//     // }
+//     return res.json({ error: 'Shop not found' });
+//     }).catch(err => console.log(err))
+//     // console.log(fb);
+//     console.log(fb);
+
+//     // if (fb !== null) return res.json(fb);
+//     // // if (fbs) {
+//     // //   return res.json(fbs);
+//     // // }
+//     // return res.json({ error: 'Shop not found' });
+//   }
+// }
+
+// export default new DisplayFeedbackController();
