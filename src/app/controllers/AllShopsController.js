c6476cbbb5148e491ad33ac32f68032b68a9194b
Artur Oliveira Gomes <>
2020-10-01 23:26:35 -0400

    quick commit

--- src/app/controllers/AllShopsController.js
+++ src/app/controllers/AllShopsController.js
@@ -8,11 +8,13 @@ class AllShopController {
     const { retail_id } = req.body;
     console.log("retail_id: ", retail_id);
     const shop = await Shop.findAll(
-      { attributes: ['id', 'retail_id', 'name', 'manager', 'phone', 'short_url'] ,
-        where: { retail_id } }
+      { attributes: ['id', 'retail_id', 'name', 'manager', 'phone', 'short_url']
+      // ,
+        // where: { retail_id }
+      }
     )
-      // .map(el => el.get({ plain: true }))
-      // .filter(s => s.retail_id === req.body.retail_id);
+      .map(el => el.get({ plain: true }))
+      .filter(s => s.retail_id === req.body.retail_id);
     console.log(shop);
     if (!shop) {
       return res.status(400).json({ error: "no shops found for this retail" });
