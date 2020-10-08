deb0591193d5b03507078075a1d562c07fd062a3
Artur Oliveira Gomes <>
2020-10-08 08:48:50 -0400

    quick commit

--- src/app/controllers/DisplayFeedbackController.js
+++ src/app/controllers/DisplayFeedbackController.js
@@ -33,7 +33,7 @@ class DisplayFeedbackController {
         return { shop_name: s.name, f };
       })
     );
-    // console.log(fb);
+    console.log(fb);
 
     if (fb !== null) return res.json(fb);
     // if (fbs) {
