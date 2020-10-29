d9cea1ff71eb5216b0695e224731f5b41554959b
Artur Oliveira Gomes <>
2020-10-29 17:54:01 -0400

    quick commit

--- src/app/controllers/ShopController.js
+++ src/app/controllers/ShopController.js
@@ -16,8 +16,8 @@ class ShopController {
 
     const shop = await Shop.findByPk(shop_id);
     if (shop) {
-      const {retail_id,name,manager,phone,short_url} = shop;
-      return res.json({retail_id,name,manager,phone,short_url});
+      const { retail_id, name, manager, phone, short_url } = shop;
+      return res.json({ retail_id, name, manager, phone, short_url });
     }
     return res.json({ error: 'Shop not found' });
   }
@@ -35,25 +35,29 @@ class ShopController {
         name: req.body.name,
         phone: req.body.phone,
         manager: req.body.manager,
-      retail_id: req.body.retail_id,
-
+        retail_id: req.body.retail_id,
         short_url: req.body.short_url,
       }))
     ) {
       return res.status(400).json({ error: Error.validation_failed });
     }
 
-    const { id, name, manager, phone,short_url } = await Shop.create({
+    // const { id, name, manager, phone, short_url } = await Shop.create({
+    await Shop.create({
       name: req.body.name,
       phone: req.body.phone,
       manager: req.body.manager,
       retail_id: req.body.retail_id,
       short_url: req.body.short_url,
-    });
+    })
+      .then(new_shop =>
+        res.status(200).json({ message: "shop added" }))
+      .catch(error => {
+        res.status(500).message(error)
+      })
 
-    return res.json({ id, name, manager, phone, short_url });
   }
-  async delete(req,res){
+  async delete(req, res) {
     const { shop_id } = req.body;
 
     const shop = await Shop.findByPk(shop_id);
